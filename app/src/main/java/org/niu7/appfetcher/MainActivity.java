12b6f7b76bf20d7f2b95d73231102b77fec185f6
kin <zhangjing@iappease.com.cn>
2018-04-04 19:01:51 +0800

    up

--- app/src/main/java/org/niu7/appfetcher/MainActivity.java
+++ app/src/main/java/org/niu7/appfetcher/MainActivity.java
@@ -1,7 +1,16 @@
 package org.niu7.appfetcher;
 
 import android.app.Activity;
+import android.content.pm.ApplicationInfo;
 import android.os.Bundle;
+
+import java.util.List;
+
+import io.reactivex.Observable;
+import io.reactivex.ObservableSource;
+import io.reactivex.Scheduler;
+import io.reactivex.functions.Function;
+import io.reactivex.schedulers.Schedulers;
 
 public class MainActivity extends Activity {
 
@@ -9,5 +18,23 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    private void fetchInstalledApp() {
+        try {
+            List<ApplicationInfo> list = getPackageManager().getInstalledApplications(0);
+            Observable.fromIterable(list)
+                    .flatMap(new Function<ApplicationInfo, ObservableSource<?>>() {
+                        @Override
+                        public ObservableSource<?> apply(ApplicationInfo applicationInfo)
+                                throws Exception {
+                            return Observable.just(applicationInfo.sourceDir);
+                        }
+                    })
+                .subscribeOn(Schedulers.io())
+                .subscribe();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
